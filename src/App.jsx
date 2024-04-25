95dd4794371cc24f011b573ee35eb9a9351830f5
im-sanny <raselparvezsanny@gmail.com>
2024-04-25 22:17:49 +0600

    files are ready to build the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <>
+      <div className="mx-auto bg-gray-700">
+        <h1 className="text-4xl font-bold text-center p-5 text-orange-600">
+          Ready to Work
+        </h1>
+      </div>
+      
+    </>
+  );
+}
+
+export default App;

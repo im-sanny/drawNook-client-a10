862a93cf8381f402c0786eade57db88dc50d502f
im-sanny <raselparvezsanny@gmail.com>
2024-04-27 20:56:17 +0600

    firebase auth added in login and registration page

--- src/FirebaseProvider/FirebaseProvider.jsx
+++ src/FirebaseProvider/FirebaseProvider.jsx
@@ -1,23 +1,58 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { createContext } from "react";
+import {
+    GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import auth from "../Firebase/firebaseConfig";
 
-export const AuthContext = createContext(null)
-const FirebaseProvider = ({children}) => {
+export const AuthContext = createContext(null);
 
-    //create user 
-    const createUser = (email, password) =>{
-       return createUserWithEmailAndPassword(auth, email, password)
-    }
+//social auth providers
+const googleProvider = new GoogleAuthProvider();
 
-    const allValues = {
-        createUser
-    }
-    return (
-        <AuthContext.Provider value={allValues}>
-            {children}
-        </AuthContext.Provider>
-    );
+const FirebaseProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+
+  //create user
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //sign in user
+  const signInUser = (email, password)=>{
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+
+  //google login
+  const googleLogin = () =>{
+    return signInWithPopup(auth, googleProvider)
+  }
+
+  //observer
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      }
+    });
+  }, []);
+
+  const allValues = {
+    createUser,
+    signInUser,
+    googleLogin,
+    
+
+  }
+  return (
+    <AuthContext.Provider value={allValues}>
+        {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default FirebaseProvider;
